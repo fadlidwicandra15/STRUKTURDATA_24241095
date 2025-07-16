ba8564f3596717e0e3459dfedf7aaad2123ce650
fadlidwicandra15 <fadlidwicandra@gmail.com>
2025-07-15 20:37:55 -0700

    Add files via upload

---
+++ uas struktur data/main.py
@@ -0,0 +1,39 @@
+# input nama customer dan tanggal belanja , simpan dalam tuple 
+nama = input("masukkan nama customer: ")
+tanggal = input("Masukkan tanggal belanja (contoh: 15-04-2025): ")
+data_customer = (nama,tanggal)
+
+# input jumlah barang yang akan dibeli
+jumlah_barang = int(input("Masukkan jumlah barang yang akan dibeli :"))
+
+# input data tiap barang, simpan dalan dictionary
+daftar_belanja= [] # list untuk menyimpan dictionary 
+
+for i in range(jumlah_barang):
+    print(f"\nBarang ke -{i+1}")
+    nama_barang = input("Nama barang:" )
+    harga_satuan = float(input("harga satuan"))
+    qty = int(input("jumlah(QTY):") )
+
+# Membuat dictionary untuk barang ini
+    barang = {
+        "nama_barang": nama_barang,
+        "harga_satuan": harga_satuan,
+        "qty": qty,
+        "total": harga_satuan * qty
+    }
+
+daftar_belanja.append(barang)  # Menambahkan ke list
+
+# 5. Tampilkan data customer, daftar belanja, dan total bayar
+print("\n===== STRUK BELANJA =====")
+print(f"Nama Customer : {data_customer[0]}")
+print(f"Tanggal       : {data_customer[1]}")
+print("\nDaftar Belanja:")
+
+total_bayar = 000
+for i, item in enumerate(daftar_belanja, start=1):
+    print(f"{i}. {item['nama_barang']} - {item['qty']} x {item['harga_satuan']} = {item['total']}")
+    total_bayar += item["total"]
+
+print(f"\nTotal Bayar: Rp {total_bayar:,.2f}")
